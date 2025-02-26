6a601e74640999e9785270871da0cebf3c7cd1e0
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-02-25 22:40:30 -0300

    refactor: update WriteCsv function to write on hr.csv file

--- controller/write_controller.go
+++ controller/write_controller.go
@@ -3,30 +3,38 @@ package controller
 import (
 	"encoding/csv"
 	"os"
+
+	"gmalheiro.com/goCSV/entity"
 )
 
-func WriteCSV() {
-	file, err := os.Create("subscribers.csv")
+func WriteCSV() error {
+	person := entity.Person{
+		Name: "Gabs",
+		CPF:  "000.000.000-00",
+		Age:  21,
+	}
+
+	file, err := os.OpenFile("./data/hr.csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	/*
+		os.O_APPEND -> Add new lines without deleting the existing ones
+		os.O_WRONLY -> Allows only writing
+		os.O_CREATE -> Creates the file if it does not exist
+		0644 -> File permissions (reading and writing for the owner, and reading for others)
+	*/
 
 	if err != nil {
-		panic(err)
+		return err
 	}
-
 	defer file.Close()
 
-	records := [][]string{
-		{"Gabs Malheiro", "gabsmalheiro@email.com"},
-		{"Teste teste", "teste@email.com"},
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Escrever a nova linha no CSV
+	err = writer.Write(entity.PersonToStringSlice(person))
+	if err != nil {
+		return err
 	}
-  
 
-  writer := csv.NewWriter(file)
-
-  defer writer.Flush()
-
-  for _, record := range records {
-    if err := writer.Write(record); err != nil {
-      panic(err)
-    }
-  }
+	return nil
 }
